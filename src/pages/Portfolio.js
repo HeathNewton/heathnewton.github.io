0856147ba8dd1b8725b43cc4cb53b3df8c6ed1f9
HeathNewton <heathnewton@gmail.com>
2018-05-25 13:03:06 +1200

    CSS tweaks

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -8,7 +8,7 @@ class Portfolio extends Component {
 				<div className="main-inner">
 		    		<h1>Portfolio</h1>
 		    		<p>Here are some of the projects I've contributed to.</p>
-		    		<ul className="projects-list nobullets nobm">
+		    		<ul className="projects-list grid nobullets nobm">
 		    			<ProjectItem item="business-invoices" title="NZ Post Business Invoices" dates="December 2016 to August 2017" desc="This functionality was to enable businesses to pay invoices online rather than by mailing a cheque." />
 		    			<ProjectItem item="svg-icons" title="SVG Icon Sprite" dates="Ongoing from February 2016" desc="Establishing an SVG icon system using the SVG Sprite technique." />
 		    			<ProjectItem item="youshop" title="YouShop Consolidation" dates="August to December 2014" desc="Building an interface to allow users to consolidate multiple YouShop parcels." />
