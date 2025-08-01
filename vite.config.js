47e6642dbc8b9534d2e6e3adfd131ee3e3edb325
Brian Milne <brian.milne@nzpost.co.nz>
2025-08-01 16:27:45 +1200

    Initial commit for new site

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/portfolio-2025/',
+  plugins: [react()],
+})
+git remote add origin git@github.com:heathnewton/heathnewton.github.io.git
+git@github.com:heathnewton/heathnewton.github.io.git
