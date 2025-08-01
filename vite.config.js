ee151d04070b289ed4d999ad6f3d3d5e07d8dd20
Brian Milne <brian.milne@nzpost.co.nz>
2025-08-01 16:48:34 +1200

    Adding react router and prettier

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-2025/',
+  base: '/',
   plugins: [react()],
 })
-git remote add origin git@github.com:heathnewton/heathnewton.github.io.git
-git@github.com:heathnewton/heathnewton.github.io.git
