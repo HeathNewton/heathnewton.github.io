7e21e300ad58d7c879c3167074a4665d8b4caebe
Brian Milne <brian.milne@nzpost.co.nz>
2025-08-19 13:35:35 +1200

    - made portfolio landing page blocks into links; - used ProjectImage component to serve thumbnails properly on portfolio landing pages; - removed focus outlines except when using a keyboard.

--- src/components/PorfolioItem.jsx
+++ src/components/PorfolioItem.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+import ProjectImage from "./ProjectImage";
 
 export default function PortfolioItem({
   item,
@@ -7,18 +9,52 @@ export default function PortfolioItem({
   desc,
   isArchive = false,
 }) {
+  const blockRef = useRef(null);
+  useEffect(() => {
+    const block = blockRef.current;
+    if (!block) return;
+    const link = block.querySelector("a");
+    if (!link) return;
+    let down = null;
+    const handlePointerDown = (e) => {
+      if (e.button === 0) {
+        down = Date.now();
+      }
+    };
+    const handlePointerUp = (e) => {
+      if (e.button === 0) {
+        const up = Date.now();
+        if (up - down < 200) {
+          if (e.ctrlKey || e.metaKey) {
+            const href = link.getAttribute("href");
+            window.open(href, "_blank");
+          } else {
+            link.click();
+          }
+        }
+      }
+    };
+    block.addEventListener("pointerdown", handlePointerDown);
+    block.addEventListener("pointerup", handlePointerUp);
+    block.style.cursor = "pointer";
+    //cleanup
+    return () => {
+      block.removeEventListener("pointerdown", handlePointerDown);
+      block.removeEventListener("pointerup", handlePointerUp);
+    };
+  }, []);
   return (
-    <li className={`project-item project-item-${item}`}>
+    <div ref={blockRef} className={`portfolio-item portfolio-item-${item}`}>
       <h2>
         <Link to={`/portfolio${isArchive ? "/archive" : ""}/${item}`}>
           {title}
         </Link>
       </h2>
-      <div className="project-item__thumbnail">
-        <img src={`/assets/images/${item}/landing-t.jpg`} alt="" />
+      <div className="portfolio-item__thumbnail">
+        <ProjectImage project={item} page="landing" thumbnail={true} />
       </div>
-      <div className="project-item__dates">{dates}</div>
-      <div className="project-item__description">{desc}</div>
-    </li>
+      <div className="portfolio-item__dates">{dates}</div>
+      <div className="portfolio-item__description">{desc}</div>
+    </div>
   );
 }
--- src/pages/PortfolioArchive.jsx
+++ src/pages/PortfolioArchive.jsx
@@ -8,34 +8,42 @@ export default function PortfolioArchive() {
         <h1>Portfolio / Archive</h1>
         <p>Here are some of the projects I contributed to from 2014 to 2018</p>
         <ul className="projects-list grid nobm" role="list">
-          <PortfolioItem
-            item="open-polytechnic"
-            title="Open Polytech Website Refresh"
-            dates="July 2018 to November 2018"
-            desc="The Open Polytechnic wanted to refresh the look and feel of their website."
-            isArchive={true}
-          />
-          <PortfolioItem
-            item="business-invoices"
-            title="NZ Post Business Invoices"
-            dates="December 2016 to August 2017"
-            desc="This functionality was to enable businesses to pay invoices online rather than by mailing a cheque."
-            isArchive={true}
-          />
-          <PortfolioItem
-            item="svg-icons"
-            title="SVG Icon Sprite"
-            dates="Ongoing from February 2016"
-            desc="Establishing an SVG icon system using the SVG Sprite technique."
-            isArchive={true}
-          />
-          <PortfolioItem
-            item="youshop"
-            title="YouShop Consolidation"
-            dates="August to December 2014"
-            desc="Building an interface to allow users to consolidate multiple YouShop parcels."
-            isArchive={true}
-          />
+          <li>
+            <PortfolioItem
+              item="open-polytechnic"
+              title="Open Polytech Website Refresh"
+              dates="July 2018 to November 2018"
+              desc="The Open Polytechnic wanted to refresh the look and feel of their website."
+              isArchive={true}
+            />
+          </li>
+          <li>
+            <PortfolioItem
+              item="business-invoices"
+              title="NZ Post Business Invoices"
+              dates="December 2016 to August 2017"
+              desc="This functionality was to enable businesses to pay invoices online rather than by mailing a cheque."
+              isArchive={true}
+            />
+          </li>
+          <li>
+            <PortfolioItem
+              item="svg-icons"
+              title="SVG Icon Sprite"
+              dates="Ongoing from February 2016"
+              desc="Establishing an SVG icon system using the SVG Sprite technique."
+              isArchive={true}
+            />
+          </li>
+          <li>
+            <PortfolioItem
+              item="youshop"
+              title="YouShop Consolidation"
+              dates="August to December 2014"
+              desc="Building an interface to allow users to consolidate multiple YouShop parcels."
+              isArchive={true}
+            />
+          </li>
         </ul>
       </div>
     </main>
